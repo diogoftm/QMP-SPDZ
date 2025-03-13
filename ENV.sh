1e6381f38c553ca506eb3899479b7f8bca568d26
Diogo Matos <diogodftm@gmail.com>
2025-03-13 13:43:11 +0000

    bump OTKeys version & add env vars to set strict key roles

--- ENV.sh
+++ ENV.sh
@@ -1,5 +1,9 @@
 # By default everything is set as an example make
-# sure to update all the variables to suit your setup
+# sure to update all the variables to suit your setup.
+# Set SENDER_STRICT_ROLE and RECEIVER_STRICT_ROLE with
+# 'tx' or 'rx' in case the KMS can only provide a specific
+# oblivious key role.
+
 export KEY_REQUEST_INTERFACE='004'
 
 # ETSI 014 specific
@@ -9,8 +13,10 @@ export ROOT_CA='ssl/root_CA.pem'
 export KMS_URI='127.0.0.1:25575'
 export SENDER_SAE_CRT='ssl/127.0.0.1.pem'
 export SENDER_SAE_KEY='ssl/127.0.0.1.key'
+export SENDER_STRICT_ROLE=''
 export RECEIVER_SAE_CRT='ssl/127.0.0.1.pem'
 export RECEIVER_SAE_KEY='ssl/127.0.0.1.key'
+export RECEIVER_STRICT_ROLE=''
 
 # Extra for ETSI 004
 export SENDER_SAE_ID='qkd//app1@aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
